a146b2e601f5a219e9208b80f2af92813a103651
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-11-14 02:12:51 +0800

    [Feat][Lex] Created SJF scheduler

--- modules/schedulers/__init__.py
+++ modules/schedulers/__init__.py
@@ -1,2 +1,3 @@
 from .scheduler import Scheduler
 from .fcfs import FCFS
+from .sjf import SJF
---
+++ modules/schedulers/sjf.py
@@ -0,0 +1,13 @@
+from .scheduler import Scheduler
+
+class SJF(Scheduler):
+    name = "Shortest Job First (SJF)"
+
+    def process_queue(self, timestamp: int, _: bool = False):
+        if self._processor.is_idle:
+            arrived_processes = self.get_arrived_processes(timestamp)
+
+            if len(arrived_processes) > 0:
+                self._ready_queue.extend(arrived_processes)
+                self._ready_queue.sort(key=lambda p: (p.burst, p.arrival, p.pid)) 
+        return self._ready_queue
