84b8b0e25331c6e7909b15af8749a17ec143b686
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-11-13 22:57:04 +0800

    [Feat][Lex] Created execution trail class for recording successive executions

---
+++ modules/execution_trail.py
@@ -0,0 +1,22 @@
+from typing import List
+from models.execution_frame import ExecutionFrame
+
+class ExecutionTrail:
+    """ A class that keeps track of a list of execution frames, and trails them successively. """
+    
+    def __init__(self, start_at: int = 0):
+        self.__start_time = start_at
+        self.__trail: List[ExecutionFrame] = []
+
+    @property
+    def trail(self):
+        return self.__trail
+
+    @property
+    def last_execution_end_time(self):
+        return self.__start_time if len(self.__trail) == 0 else self.__trail[-1].end
+
+    def add_frame(self, name: str, timestamp: int):
+        """ Add a frame to the execution trail and its corresponding timestamp. """
+        frame = ExecutionFrame(name, self.last_execution_end_time, timestamp)
+        self.__trail.append(frame)
