6bf81e8680590b309f8db41e7c22f77bd2878223
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-11-25 14:41:41 +0800

    [Feat][Lex] Added a way to add footers in tables, and singularized headers to header

--- views/table.py
+++ views/table.py
@@ -2,26 +2,34 @@ from typing import List, Any
 from views import View
 
 class TableView(View):
-    def __init__(self, min_cell_width: int = 5, headers: List[str] = []):
+    def __init__(self, min_cell_width: int = 5, header: List[str] = [], footer: List[str] = []):
         super().__init__(min_cell_width)
         self.__data: List[List[Any]] = []
-        self.__headers: List[str] = headers
+        self.__header: List[str] = header
+        self.__footer: List[str] = footer
 
-        if len(headers) > 0: 
-            self._adjust_cell_sizes_to_fit(*headers)
+        if len(header) > 0: 
+            self._adjust_cell_sizes_to_fit(*header)
+        if len(footer) > 0: 
+            self._adjust_cell_sizes_to_fit(*footer)
 
     def __str__(self):
         table = ""
         sep_line = self._create_separator_line()
 
-        if len(self.__headers) > 0:
+        if len(self.__header) > 0:
             table += sep_line + "\n"
-            table += self._format_row(self.__headers) + "\n"
+            table += self._format_row(self.__header) + "\n"
 
         table += sep_line + "\n"
         for data in self.__data:
             table += self._format_row(data) + "\n"
         table += sep_line
+        
+        if len(self.__footer) > 0:
+            table += "\n"
+            table += self._format_row(self.__footer) + "\n"
+            table += sep_line
         
         return table
     
