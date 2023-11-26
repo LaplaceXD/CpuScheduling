8e5aa118c6246051c2e879fd6986667fbbcc8491
Jonh Alexis Buot <ja.buotph@gmail.com>
2023-11-26 20:03:13 +0800

    [Feat][Lex] Mounted memory management simulations to main

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import os
 
 from scheduling import main as scheduling_main
+from memory_managing import main as memory_managing_main
 from utils.io import input_bounded_num
 from views import View
 
 def main():
     choices = [
         "CPU Scheduling",
+        "Memory Management"
     ]
     
     entries = [
         scheduling_main,
+        memory_managing_main
     ]
 
     print("===== Operating System Simulator =====")
@@ -19,15 +22,14 @@ def main():
     print("What do you want to simulate?")
     choice = input_bounded_num("Choice: ", max=len(choices))
     
-
-    is_interrupted = False 
-    while not is_interrupted:
+    is_running = True 
+    while is_running:
         os.system("cls")
         entries[choice - 1]()
         
         status = input("\n\nContinue (type quit to exit)...")
         if status and status.lower() == "quit":
-            is_interrupted = True
+            is_running = False
 
 if __name__ == "__main__":
     main()
